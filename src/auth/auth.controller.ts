f31848b6303d1249f528200895e7f5fa0dafded2
Jun Liu <l.jun1717@gmail.com>
2022-11-28 21:02:41 +0800

    (feat) add signup logic to create user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Post, Req } from "@nestjs/common";
+import { Body, Controller, Get, ParseIntPipe, Post, Req } from "@nestjs/common";
 import { Request } from "express";
 import { AuthDto } from "src/dto";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthService } from "./auth.service";
+import * as argon from "argon2";
 
 @Controller('auth')
 export class AuthController{
@@ -10,11 +11,23 @@ export class AuthController{
     }
 
     @Post('signup')
-    signup(@Body() dto: AuthDto) {
+    async signup(@Body() dto: AuthDto) {
+        // generate password
+        const hash = await argon.hash(dto.password);
+
+        // save the user to db
+        const user = await this.prismaService.user.create({
+            data: {
+                email: dto.email,
+                hash,
+            }
+        })
+
+        // return user
         console.log({
-            dto
+            user
         });
-        return "sign up"
+        return user
     }
 
     // @Post('login')
