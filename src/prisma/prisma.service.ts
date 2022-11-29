c756929a930ccc447306d12d4726d30930568256
Jun Liu <l.jun1717@gmail.com>
2022-11-29 23:17:14 +0800

    (feat) use NestJsConfig

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
+    constructor(private configSerivce: ConfigService) {
         super({
             datasources: {
                 db: {
-                    url:  process.env.DATABASE_URL
+                    url:  configSerivce.get("DATABASE_URL")
                 }
             }
         })
