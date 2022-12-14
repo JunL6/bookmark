b9c0d96407950f5c72246e61cf495f781007243a
Jun <l.jun1717@gmail.com>
2022-12-14 23:58:43 +0800

    (refactor) avoid magic string by creating JwtAuthGuard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Post, Req, Session, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { Request } from "express";
+import { JwtAuthGuard } from "./JwtAuthGuard";
 import { Roles } from "./roles.decorator";
 import { RolesGuard } from "./roles.guard";
 
 @Controller("user")
 export class UserController {
-    @UseGuards(AuthGuard("jwt"), RolesGuard)
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Roles('USER')
     @Get("me")
     getMe(@Req() request: Request) {
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtAuthGuard } from './JwtAuthGuard';
 import { RolesGuard } from './roles.guard';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
     controllers: [UserController],
-    providers: [UserService, RolesGuard],
+    providers: [UserService, RolesGuard, JwtAuthGuard],
     exports: [UserService]
 })
 export class UserModule {}
