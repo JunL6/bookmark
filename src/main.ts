a9073a5605b74d6c2e7812cd31dc31dec97870b1
Jun <l.jun1717@gmail.com>
2023-01-13 14:32:07 +0800

    (feat) add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 dotenv.config();
 
 async function bootstrap() {
@@ -9,6 +10,16 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }));
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Bookmark Project')
+    .setDescription('Bookmark Project API')
+    .setVersion('1.0')
+    .addTag('bookmark')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
